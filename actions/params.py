bc90aac88f477a852cf94f1b9c9247581aabaffe
Sahil Mondol <sahilmondol95@gmail.com>
2019-08-02 16:06:19 +0530

    miscellaneous changes

--- actions/params.py
+++ actions/params.py
@@ -4,4 +4,5 @@ from st2common.runners.base_action import Action
 
 class Greet(Action):
     def run(self, message):
-        print(f"Welcome {message}")
+		output = "Welcome" + message
+        print(output)
