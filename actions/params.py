351f31219e4bf447ee94f0c7ab641403855bfb64
Sahil Mondol <sahilmondol95@gmail.com>
2019-08-02 15:36:39 +0530

    miscellaneous changes

--- actions/params.py
+++ actions/params.py
@@ -4,4 +4,4 @@ from st2common.runners.base_action import Action
 
 class Greet(Action):
     def run(message):
-        print('Welcome '+message+' to StackStorm!')
+        print('Welcome ',message)
