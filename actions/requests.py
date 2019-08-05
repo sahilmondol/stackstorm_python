359c937ab6573f7778f19b7062c1c171f5c7ee42
Sahil Mondol <sahilmondol95@gmail.com>
2019-08-05 11:52:33 +0530

    miscellaneous changes

--- actions/requests.py
+++ actions/requests.py
@@ -4,8 +4,8 @@ import sys
 from st2common.runners.base_action import Action
 
 class requests_api(Action):
-    def run(self, url):
-        print("URL is : {0}".format(url))
+	def run(self, url):
+		print("URL is : {0}".format(url))
 		resp = requests.get(url=url)
-		data = resp.json()
+		data = resp.json()		        
 		print(data)
