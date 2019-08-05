3576d1a696c98e38a20fcc81ddb36d0ae2e20d6d
Sahil Mondol <sahilmondol95@gmail.com>
2019-08-05 13:16:18 +0530

    miscelleneous changes

--- actions/requests.py
+++ actions/requests.py
@@ -6,4 +6,4 @@ from st2common.runners.base_action import Action
 
 class requests_api(Action):
 	def run(self,message):
-        print(json.load(urllib.urlopen(url)))
+		print(json.load(urllib.urlopen(url)))
