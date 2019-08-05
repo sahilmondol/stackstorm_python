d4285feccae369866bf91e65a6ac3080304357ae
Sahil Mondol <sahilmondol95@gmail.com>
2019-08-05 15:59:33 +0530

    miscelleneous changes

---
+++ actions/requests_new.py
@@ -0,0 +1,11 @@
+import requests
+import sys
+
+from st2common.runners.base_action import Action
+
+class requests_api(Action):
+	def run(self,url):
+		print("URL is: {0}".format(url))
+		resp = requests.get(url)
+		data = resp.json()
+		print(data)
