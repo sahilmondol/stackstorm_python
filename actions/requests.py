124aaf95cda44ed168e68d05abccceb16392a35e
Sahil Mondol <sahilmondol95@gmail.com>
2019-08-05 12:29:46 +0530

    miscellaneous changes

--- actions/requests.py
+++ actions/requests.py
@@ -4,7 +4,7 @@ import sys
 from st2common.runners.base_action import Action
 
 class requests_api(Action):
-	def run(self):
+	def run(self,message):
 		temp_url = "https://raw.githubusercontent.com/Azure/azure-quickstart-templates/master/101-vm-simple-linux/azuredeploy.json"
         req = requests.get(temp_url)
         template = req.json()
