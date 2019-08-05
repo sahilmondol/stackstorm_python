3fe0a4f4b9811c4ac7144a99459a21c5582fbcfb
Sahil Mondol <sahilmondol95@gmail.com>
2019-08-05 13:19:11 +0530

    miscelleneous changes

--- actions/requests.py
+++ actions/requests.py
@@ -6,4 +6,5 @@ from st2common.runners.base_action import Action
 
 class requests_api(Action):
 	def run(self,message):
+		url="https://raw.githubusercontent.com/Azure/azure-quickstart-templates/master/101-vm-simple-linux/azuredeploy.json"
 		print(json.load(urllib.urlopen(url)))
